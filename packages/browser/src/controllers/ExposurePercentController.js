b1bd93a351802e869b9a49c0073e6738bc1b302b
Wesley Petit <wesley.petit.lemoine@gmail.com>
2023-08-17 13:00:40 +0200

    Add daily style for exposure percent

--- packages/browser/src/controllers/ExposurePercentController.js
+++ packages/browser/src/controllers/ExposurePercentController.js
@@ -1,5 +1,6 @@
 import { THREE, itowns } from '@ud-viz/browser';
 import { AggregateController } from './AggregateController';
+import { TimeScales } from '../utils/Time';
 
 /* Aggregate Controller responsible of exposurePercent aggregate */
 export class ExposurePercentController extends AggregateController {
@@ -14,12 +15,28 @@ export class ExposurePercentController extends AggregateController {
    * @returns {itowns.Style} Itowns style depending on monthly/daily exposure percent
    */
   getStyle() {
+    /* Lerp between minimal and maximal colors depending on monthly exposure */
+    if (this.timeScale == TimeScales.Month) {
+      return new itowns.Style({
+        fill: {
+          color: function (feature) {
+            const alpha =
+              feature.getInfo().batchTable.monthlyExposurePercent / 100;
+
+            const yellow = new THREE.Color('rgb(255, 222, 0)');
+            const red = new THREE.Color('rgb(210, 0, 26)');
+
+            return yellow.lerp(red, alpha);
+          },
+        },
+      });
+    }
+
+    /* Lerp between minimal and maximal colors depending on daily exposure */
     return new itowns.Style({
       fill: {
         color: function (feature) {
-          /* Lerp between minimal and maximal colors depending on monthly exposure */
-          const alpha =
-            feature.getInfo().batchTable.monthlyExposurePercent / 100;
+          const alpha = feature.getInfo().batchTable.dailyExposurePercent / 100;
 
           const yellow = new THREE.Color('rgb(255, 222, 0)');
           const red = new THREE.Color('rgb(210, 0, 26)');
